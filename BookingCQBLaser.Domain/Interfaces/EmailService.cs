5d227e610f44fac07184123615085c91433bfe87
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-04 18:21:45 +0200

    fixed email tos and price

--- BookingCQBLaser.Domain/Interfaces/EmailService.cs
+++ BookingCQBLaser.Domain/Interfaces/EmailService.cs
@@ -19,12 +19,13 @@ namespace BookingCQBLaser.Domain.Interfaces
 
         public async Task SendBookingConfirmationAsync(Booking booking, int totalCost, int depositAmount, int remainingBalance)
         {
+            
             if (string.IsNullOrWhiteSpace(_smtpOptions.SenderEmail))
                 throw new InvalidOperationException("SmtpOptions: SenderEmail is missing or null.");
 
             if (string.IsNullOrWhiteSpace(booking.Customer?.Email))
                 throw new InvalidOperationException("Booking: Customer Email is missing or null.");
-
+            totalCost+=4;
             var email = new MimeMessage();
 
             email.From.Add(new MailboxAddress(_smtpOptions.SenderName, _smtpOptions.SenderEmail));
@@ -42,7 +43,7 @@ namespace BookingCQBLaser.Domain.Interfaces
                         <ul>
                             <li><strong>Data i godzina rezerwacji:</strong> {booking.StartTime:dd.MM.yyyy HH:mm}</li>
                             <li><strong>Suma kosztów:</strong> {totalCost} PLN</li>
-                            <li><strong>Wpłacono:</strong> {paidAmount} PLN</li>
+                            <li><strong>Zapłacono:</strong> {paidAmount} PLN</li>
                             <li><strong>Do zapłaty na miejscu:</strong> {remainingBalance} PLN (TYLKO GOTÓWKĄ)</li>
                         </ul>
                         
@@ -53,7 +54,8 @@ namespace BookingCQBLaser.Domain.Interfaces
                             <strong>Telefon:</strong> 509 595 199
                         </p>
 
-                        <h3>Dokumenty do pobrania</h3>
+                        <h3>Dokumenty do pobrania - należy wypełnić i przekazać intruktorowi w dniu imprezy</h3>
+                        <p>W przypadku uczestnictwa osób niepełnoletnich wymagana jest zgoda rodzica/opiekuna oraz oświadczenie opiekuna grupy.</p>
                         <ul>
                             <li><a href=""https://comboarena.pl/wp-content/uploads/oswiadczenieopiekunagrupy.pdf"">Oświadczenie opiekuna grupy</a></li>
                             <li><a href=""https://comboarena.pl/wp-content/uploads/zgodarodzica.pdf"">Zgoda rodzica</a></li>
@@ -63,11 +65,10 @@ namespace BookingCQBLaser.Domain.Interfaces
                         <ul>
                             <li>Wiek min. 10 lat</li>
                             <li>Min. liczba uczestników: 10 osób dla pakietów (S1/U1) lub 8 osób dla pozostałych</li>
-                            <li>Zadatek 300 PLN bezzwrotny</li>
-                            <li>Płatność dopłaty na miejscu tylko gotówką</li>
+                            <li>Zadatek 300 PLN</li>
+                            <li>Płatność pozostałej kwoty na miejscu tylko gotówką</li>
                             <li>Punktualność wymagana</li>
-                            <li>Trzeźwość i Bezpieczeństwo jest priorytetem</li>
-                            <li>Osoby niepełnoletnie potrzebują zgód od rodziców/opiekunów</li>
+                            <li>Osoby będące pod wpływem alkoholu nie będą wpuszczane na teren obiektu</li>
                         </ul>
                         <br/>
                         <p>Z pozdrowieniami,<br/>Zespół Combo Arena</p>";
