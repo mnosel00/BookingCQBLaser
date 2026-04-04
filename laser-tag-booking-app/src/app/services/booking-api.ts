ade41590b0bdcad16555800a323cb3833b9893e4
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-04 20:57:39 +0200

    ex

--- laser-tag-booking-app/src/app/services/booking-api.ts
+++ laser-tag-booking-app/src/app/services/booking-api.ts
@@ -8,12 +8,13 @@ import { TimeSlot, CreateBookingRequest, PackageType } from '../models/booking.m
 })
 export class BookingApiService {
   private readonly httpClient = inject(HttpClient);
-  private readonly apiUrl = 'https://localhost:7083/api/bookings';
+  //private readonly apiUrl = 'https://localhost:7083/api/bookings';
+  private readonly apiUrl = 'https://nontumultuous-rosamaria-discernably.ngrok-free.dev/api/bookings';
 
-  getAvailableSlots(date: string, packageType: PackageType): Observable<TimeSlot[]> {
-    const url = `${this.apiUrl}/available-slots?date=${encodeURIComponent(date)}&package=${packageType}`;
-    return this.httpClient.get<TimeSlot[]>(url);
-  }
+  // getAvailableSlots(date: string, packageType: PackageType): Observable<TimeSlot[]> {
+  //   const url = `${this.apiUrl}/available-slots?date=${encodeURIComponent(date)}&package=${packageType}`;
+  //   return this.httpClient.get<TimeSlot[]>(url);
+  // }
 
   createBooking(request: CreateBookingRequest): Observable<{ bookingId: string }> {
     return this.httpClient.post<{ bookingId: string }>(this.apiUrl, request);
